6925767658bfe6fad00fdcf7796633c7d08c5a2e
priyappatil <priyappatil201@gmail.com>
2021-09-24 19:01:41 +0530

    Passing Functional Props and Two-Way Binding

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Person = ({name, age}) => {         // Here, name and age are the props.
+const Person = ({name, age, myClick, changed}) => {         // Here, name and age are the props.
     return (
-        <div className = "container">
-            <p className = "lead"> Hi, I am {name} and I am {age} years old.</p>
+        <div className = "container-fluid">
+            <p onClick = {myClick} className = "lead"> Hi, I am {name} and I am {age} years old.</p>
+            <input type="text" placeholder = "Enter the name" onChange = {changed} />
         </div>
     )
 }
